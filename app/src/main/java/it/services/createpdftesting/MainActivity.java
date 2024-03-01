952a117faf24d9a74f9ff138c22e0c2b3b0ddf54
Bhushan <bhushan.lamba@ciphersquare.tech>
2024-03-01 12:20:10 +0530

    test

--- app/src/main/java/it/services/createpdftesting/MainActivity.java
+++ app/src/main/java/it/services/createpdftesting/MainActivity.java
@@ -33,9 +33,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnCreatePdf = findViewById(R.id.btn_create_pdf);
